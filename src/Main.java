e87b1be9c83474d91b7c807c200ea510b75635bb
andrei_nagin <actifen@list.ru>
2023-04-23 01:33:28 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String folderPath = "C:/Users/actif/Desktop/webinar";
+        File file = new File(folderPath);
+
+        System.out.println(file.length());
+
+    }
+
+    public static long getFolderSize(File folder) {
+        if(folder.isFile()) {
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
+    }
+
+}
