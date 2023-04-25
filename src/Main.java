5c98751919e30b1406e79ea0a7bf48778eceb64e
viktoriia_grebeniuk <proximacccc@gmail.com>
2023-04-25 19:18:14 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:/Users/actif/Desktop/webinar";
+
+        String folderPath = "C:/Users/proximacc/Desktop/webinar";
+
         File file = new File(folderPath);
 
-        System.out.println(file.length());
+        FolderSizeCalculator calculator =  new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = (long) pool.invoke(calculator);
+        System.out.println(size);
+        //System.out.println(getFolderSize(file));
 
     }
-
     public static long getFolderSize(File folder) {
         if(folder.isFile()) {
-
             return folder.length();
         }
-
         long sum = 0;
         File[] files = folder.listFiles();
-        for (File file : files) {
+        for (File file : files)
+        {
             sum += getFolderSize(file);
         }
         return sum;
     }
-
 }
