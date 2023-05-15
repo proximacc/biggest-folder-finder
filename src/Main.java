166511ec220cf8d639c6ecb34cbbc70197fee310
viktoriia_grebeniuk <proximacccc@gmail.com>
2023-05-15 14:31:04 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
+
+    private static char [] sizeMultipliers = {'B','K', 'M', 'G', 'T'};
+
     public static void main(String[] args) {
 
         String folderPath = "C:/Users/proximacc/Desktop/webinar";
@@ -13,6 +17,8 @@ public class Main {
         long size = (long) pool.invoke(calculator);
         System.out.println(size);
         //System.out.println(getFolderSize(file));
+        System.out.println(getHumanReadableSize(size));
+        System.out.println(getSizeFromHumanReadable("259 Mb"));
 
     }
     public static long getFolderSize(File folder) {
@@ -26,5 +32,36 @@ public class Main {
             sum += getFolderSize(file);
         }
         return sum;
+    }
+
+    public static String getHumanReadableSize (long size) {
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            double value = size / Math.pow(1024, i);
+            if (value < 1024) {
+                return Math.round(value) + "" + sizeMultipliers[i] + (i > 0 ? "b" : "");
+            }
+        }
+        return "Very big!";
+    }
+
+    public static long getSizeFromHumanReadable (String size) {
+        HashMap<Character, Integer> char2multiplier = getMultipliers();
+        char sizeFactor = size
+                .replaceAll("[0-9\\s+]+", "")
+                .charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        long length = multiplier * Long.valueOf(size.replaceAll("[^0-9]", ""));
+        return length;
+    }
+
+    private static HashMap<Character, Integer> getMultipliers() {
+        HashMap<Character, Integer> char2multiplier = new HashMap<>();
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            char2multiplier.put(
+                    sizeMultipliers[i],
+                    (int) Math.pow(1024, i)
+            );
+        }
+        return char2multiplier;
     }
 }
